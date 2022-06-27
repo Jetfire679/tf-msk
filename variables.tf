923f332594e6ff034ffc329c02b95cb7356cc976
jetfire679 <jetfire679@gmail.com>
2022-06-27 16:36:16 -0400

    SG updates

--- variables.tf
+++ variables.tf
@@ -87,24 +87,9 @@ variable "kafka-client-ingress-rules" {
       cidr_blocks = ["76.181.232.36/32"]
     },
     {
-      port        = 9092
-      proto       = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-    },
-    {
-      port        = 9094
-      proto       = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-    },
-    {
-      port        = 2181
-      proto       = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
-    },
-    {
-      port        = 2182
-      proto       = "tcp"
-      cidr_blocks = ["10.0.0.0/16"]
+      port        = 0
+      proto       = "-1"
+      cidr_blocks = ["10.0.0.0/8"]
     }
   ]
 }
